1eadf26bff8c8fc4155d0f9f5c09b9815d35962b
405758847 <405758847@163.com>
2019-06-30 23:18:04 +0800

    添加项目文件。

---
+++ SocketClient-20190629/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SocketClient_20190629
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<IPEndPoint, Socket> Sockets = null;
+        TcpNet tcpNet = new TcpNet();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            Socket socket = null;
+            socket = tcpNet.ClientConnect(ipe);
+            Sockets.Add(ipe, socket);
+            string text = textBox1.Text + textBox2.Text;
+            textBox3.AppendText("已连接至：" + text);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            IPEndPoint ipe=new IPEndPoint(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            bool getSocket=Sockets.TryGetValue(ipe,out Socket socket);
+            if (!getSocket)
+            {
+                textBox3.AppendText("连接不存在.");
+                return;
+            }
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+            string text = textBox1.Text + textBox2.Text;
+            textBox3.AppendText("已关闭连接：" + text);
+            return;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(textBox4.Text);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            bool getSocket = Sockets.TryGetValue(ipe, out Socket socket);
+            if (!getSocket)
+            {
+                textBox3.AppendText("连接不存在.");
+                return;
+            }
+            tcpNet.ClientSend(socket, bytes);
+
+            string str = ipe.ToString();
+            str += ":" + textBox4.Text;
+            textBox3.AppendText(str);
+            return;
+        }
+    }
+}
---
+++ SocketClient-20190629/TcpNet.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient_20190629
+{
+    class TcpNet
+    {
+        private List<Socket> _sockets;
+
+        public List<Socket> Sockets
+        {
+            get { return _sockets; }
+        }
+        private int socketBufferSize=1024;
+        public Socket ClientConnect(IPEndPoint ipe)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(ipe);
+                _sockets.Add(socket);
+                return socket;
+
+            }catch(Exception ex)
+            {
+                return null;
+            }
+            
+        }
+        public byte[] ClientReceive(Socket socket,int length)
+        {
+            
+            if (length <= 0) return null;
+            byte[] bytes = new byte[length];
+            int receiveCount=0;
+            while (receiveCount < length)
+            {
+                try
+                {
+                    int receiveLength = (length - receiveCount) > socketBufferSize ? socketBufferSize : (length - receiveCount);
+                    receiveCount += socket.Receive(bytes,receiveCount,receiveLength,0);
+                }
+                catch
+                {
+                    _sockets.Remove(socket);
+                    socket?.Close();
+                    return null;
+                }
+            }
+            return bytes;
+            
+        }
+        public bool ClientSend(Socket socket,byte[] bytes)
+        {
+            if (bytes == null) return false;
+            try
+            {
+                socket.Send(bytes);
+                return true;
+            }
+            catch
+            {
+                _sockets.Remove(socket);
+                socket?.Close();
+                return false;
+            }
+        }
+
+    }
+}
